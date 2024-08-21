8ce13f3d9ccc3de8aa69807bb0ae720e00333da8
ZakTan <Ztanksley@luc.edu>
2024-08-21 12:11:39 -0700

    Conformed ColorTableVC to Delegate and datasorce. Added extension to place both, for cleaner code. performedSegue to tableview `ToColorsDetailVC` segue

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -7,26 +7,26 @@
 
 import UIKit
 
-class ColorsTableVC: UIViewController {
-
+class ColorsTableVC: UIViewController{
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    
+}
+// Added extension for cleaner code
+extension ColorsTableVC: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
 }
